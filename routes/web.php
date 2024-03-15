fcaf7c3fae2b1269317f8f8938d4ebff7949abe2
zhenya457 <derkachzhenya>
2024-03-15 22:38:48 +0200

    v1

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('about', 'about')->name('about');
+
+
+ 
